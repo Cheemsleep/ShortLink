e0d4be83e3f2f0f4c60cef126cd45b1aaf21d69a
Cheemesleep <1139065936@qq.com>
2024-02-04 15:57:22 +0800

    feature: 开发短链接后管系统用户注册相关功能

--- admin/src/main/java/com/cfl/shortlink/admin/service/UserService.java
+++ admin/src/main/java/com/cfl/shortlink/admin/service/UserService.java
@@ -3,6 +3,7 @@ package com.cfl.shortlink.admin.service;
 
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.cfl.shortlink.admin.dao.entity.UserDO;
+import com.cfl.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.cfl.shortlink.admin.dto.resp.UserRespDTO;
 
 /**
@@ -15,4 +16,17 @@ public interface UserService extends IService<UserDO> {
      * @return 用户返回实体
      */
     UserRespDTO getUserByUsername(String username);
+
+    /**
+     * 查询用户名是否已经存在
+     * @param username
+     * @return 存在返回 false 不存在返回 true
+     */
+    Boolean hasUsername(String username);
+
+    /**
+     * 注册用户
+     * @param requestParam
+     */
+    void register(UserRegisterReqDTO requestParam);
 }
--- admin/src/main/java/com/cfl/shortlink/admin/service/impl/UserServiceImpl.java
+++ admin/src/main/java/com/cfl/shortlink/admin/service/impl/UserServiceImpl.java
@@ -1,5 +1,6 @@
 package com.cfl.shortlink.admin.service.impl;
 
+import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
@@ -7,8 +8,11 @@ import com.cfl.shortlink.admin.common.convention.exception.ClientException;
 import com.cfl.shortlink.admin.common.enums.UserErrorCodeEnum;
 import com.cfl.shortlink.admin.dao.entity.UserDO;
 import com.cfl.shortlink.admin.dao.mapper.UserMapper;
+import com.cfl.shortlink.admin.dto.req.UserRegisterReqDTO;
 import com.cfl.shortlink.admin.dto.resp.UserRespDTO;
 import com.cfl.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
+import org.redisson.api.RBloomFilter;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 
@@ -17,7 +21,11 @@ import org.springframework.stereotype.Service;
  * 用户接口实现层
  */
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements UserService {
+
+    private final RBloomFilter<String> userRegisterCachePenetrationBloomFilter;
+
     @Override
     public UserRespDTO getUserByUsername(String username) {
         LambdaQueryWrapper<UserDO> queryWrapper = Wrappers.lambdaQuery(UserDO.class)
@@ -29,5 +37,22 @@ public class UserServiceImpl extends ServiceImpl<UserMapper, UserDO> implements 
         }
         BeanUtils.copyProperties(userDO, result);
         return result;
+    }
+
+    @Override
+    public Boolean hasUsername(String username) {
+        return !userRegisterCachePenetrationBloomFilter.contains(username); //应用布隆过滤器
+    }
+
+    @Override
+    public void register(UserRegisterReqDTO requestParam) {
+        if (!hasUsername(requestParam.getUsername())) {
+            throw new ClientException(UserErrorCodeEnum.USER_NAME_EXIST);
+        }
+        int inserted = baseMapper.insert(BeanUtil.toBean(requestParam, UserDO.class));
+        if (inserted < 1) {
+            throw new ClientException(UserErrorCodeEnum.USER_SAVE_ERROR);
+        }
+        userRegisterCachePenetrationBloomFilter.add(requestParam.getUsername());
     }
 }
