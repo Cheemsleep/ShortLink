a929589fd572e1586559dffb0899be9c970fd340
Cheemesleep <1139065936@qq.com>
2024-02-07 12:24:07 +0800

    feature: 开发短链接后管分组功能列表查询

--- admin/src/main/java/com/cfl/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/cfl/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -4,6 +4,7 @@ import cn.hutool.core.bean.BeanUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.cfl.shortlink.admin.common.biz.user.UserContext;
 import com.cfl.shortlink.admin.dao.entity.GroupDO;
 import com.cfl.shortlink.admin.dao.mapper.GroupMapper;
 import com.cfl.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
@@ -32,6 +33,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
         GroupDO groupDO = GroupDO.builder()
                         .gid(gid)
+                        .username(UserContext.getUsername())
                         .name(groupName)
                         .sortOrder(0)
                         .build();
@@ -41,10 +43,9 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     @Override
     public List<ShortLinkGroupRespDTO> listGroup() {
-        // TODO 从上下文获取用户名
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getDelFlag, 0)
-                .eq(GroupDO::getUsername, "test")
+                .eq(GroupDO::getUsername, UserContext.getUsername())
                 .orderByDesc(GroupDO::getSortOrder, GroupDO::getUpdateTime);
 
         List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
@@ -56,7 +57,7 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
                 //TODO 设置用户名
-                .eq(GroupDO::getUsername, "test");
+                .eq(GroupDO::getUsername, UserContext.getUsername());
 
         GroupDO data = baseMapper.selectOne(queryWrapper);
         return data == null;
