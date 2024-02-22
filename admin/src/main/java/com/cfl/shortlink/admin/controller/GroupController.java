c5d9342b9dc4c9c089efe71e1a5ccf8f41963e04
Cheemesleep <1139065936@qq.com>
2024-02-22 09:51:56 +0800

    feature: 修改短链接后管映射路径

--- admin/src/main/java/com/cfl/shortlink/admin/controller/GroupController.java
+++ admin/src/main/java/com/cfl/shortlink/admin/controller/GroupController.java
@@ -27,7 +27,7 @@ public class GroupController {
     /**
      * 新增短链接分组
      */
-    @PostMapping("/api/short-link/v1/group")
+    @PostMapping("/api/short-link/admin/v1/group")
     public Result<Void> save(@RequestBody ShortLinkGroupSaveReqDTO requestParam) {
         groupService.saveGroup(requestParam.getName());
         return Results.success();
@@ -36,7 +36,7 @@ public class GroupController {
     /**
      * 查询短链接分组集合
      */
-    @GetMapping("/api/short-link/v1/group")
+    @GetMapping("/api/short-link/admin/v1/group")
     public Result<List<ShortLinkGroupRespDTO>> listGroup() {
 
         return Results.success(groupService.listGroup());
@@ -45,7 +45,7 @@ public class GroupController {
     /**
      * 修改短链接分组
      */
-    @PutMapping("/api/short-link/v1/group")
+    @PutMapping("/api/short-link/admin/v1/group")
     public Result<Void> updateGroup(@RequestBody ShortLinkGroupUpdateReqDTO requestParam) {
         groupService.updateGroup(requestParam);
         return Results.success();
@@ -54,7 +54,7 @@ public class GroupController {
     /**
      * 删除短链接分组
      */
-    @DeleteMapping("/api/short-link/v1/group")
+    @DeleteMapping("/api/short-link/admin/v1/group")
     public Result<Void> deleteGroup(@RequestParam String gid) {
         groupService.deleteGroup(gid);
         return Results.success();
@@ -63,7 +63,7 @@ public class GroupController {
     /**
      * 重新排序
      */
-    @PostMapping("/api/short-link/v1/group/sort")
+    @PostMapping("/api/short-link/admin/v1/group/sort")
     public Result<Void> sortGroup(@RequestBody List<ShortLinkGroupSortReqDTO> requestParam) {
         groupService.sortGroup(requestParam);
         return Results.success();
