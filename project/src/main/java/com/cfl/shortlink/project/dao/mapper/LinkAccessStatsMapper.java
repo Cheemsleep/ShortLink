2f62a2d2ba89ff025bc2663b79041f4a1851f1a4
Cheemesleep <1139065936@qq.com>
2024-02-28 16:03:17 +0800

    refactor: 修改SQL语句逻辑

--- project/src/main/java/com/cfl/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
+++ project/src/main/java/com/cfl/shortlink/project/dao/mapper/LinkAccessStatsMapper.java
@@ -13,24 +13,10 @@ public interface LinkAccessStatsMapper extends BaseMapper<LinkAccessStatsDO> {
     /**
      * 记录基础访问监控数据
      */
-    @Insert("INSERT INTO t_link_access_stats " +
-            "(" +
-            "    full_short_url, gid, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag" +
-            ")\n" +
-            "VALUES(" +
-            "          #{linkAccessStats.fullShortUrl}, " +
-            "          #{linkAccessStats.gid}, " +
-            "          #{linkAccessStats.date}, " +
-            "          #{linkAccessStats.pv}, " +
-            "          #{linkAccessStats.uv}, " +
-            "          #{linkAccessStats.uip}, " +
-            "          #{linkAccessStats.hour}, " +
-            "          #{linkAccessStats.weekday}, " +
-            "          NOW(), " +
-            "          NOW(), " +
-            "          0" +
-            ")\n" +
-            "ON DUPLICATE KEY UPDATE pv = #{linkAccessStats.pv} + 1, uv = #{linkAccessStats.uv} + 2, uip = #{linkAccessStats.uip} + 3;")
+    @Insert("INSERT INTO t_link_access_stats (full_short_url, gid, date, pv, uv, uip, hour, weekday, create_time, update_time, del_flag) " +
+            "VALUES( #{linkAccessStats.fullShortUrl}, #{linkAccessStats.gid}, #{linkAccessStats.date}, #{linkAccessStats.pv}, #{linkAccessStats.uv}, #{linkAccessStats.uip}, #{linkAccessStats.hour}, #{linkAccessStats.weekday}, NOW(), NOW(), 0) ON DUPLICATE KEY UPDATE pv = pv +  #{linkAccessStats.pv}, " +
+            "uv = uv + #{linkAccessStats.uv}, " +
+            " uip = uip + #{linkAccessStats.uip};")
     void shortLinkStats(@Param("linkAccessStats") LinkAccessStatsDO linkAccessStats);
 
 }
